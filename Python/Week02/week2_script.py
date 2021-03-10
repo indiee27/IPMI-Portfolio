2e253c952d6cdc7b366d54662a3a599b684293f3
indiee27 <rmapile@ucl.ac.uk>
2021-03-10 13:38:04 +0000

    week02 script completed

--- Python/Week02/week2_script.py
+++ Python/Week02/week2_script.py
@@ -62,32 +62,42 @@ from utils2 import calcSSD
 theta = np.arange(-90, 91, 1)
 SSDs = np.zeros((theta.size, 4))
 num_pix_x, num_pix_y = ct_img_int8.shape
+rotation_point = [10,10]
+
 for n in range(theta.size):
   
   # CREATE AFFINE MATRIX AND CORRESPONDING DEFORMATION FIELD
-  aff_mat = 
-  def_field = 
+  aff_mat = affineMatrixForRotationAboutPoint(n,rotation_point)
+  def_field = defFieldFromAffineMatrix(aff_mat, num_pix_x, num_pix_y)
+
   # RESAMPLE THE IMAGES
-  ct_img_int8_resamp = 
-  ct_img_int16_resamp = 
-  mr_img_int16_resamp = 
+  ct_img_int8_resamp = resampImageWithDefField(ct_source_img_8, def_field)
+  ct_img_int16_resamp = resampImageWithDefField(ct_source_img_16, def_field)
+  mr_img_int16_resamp = resampImageWithDefField(mr_source_img, def_field)
   
   # DISPLAY THE TRANSFORMED 8 BIT CT IMAGE
-  
+  #plt.figure()
+  #dispImage(ct_img_int8_resamp)
+
   #add a short pause so the figure display updates
-  plt.pause(0.05)
+  #plt.pause(0.05)
   
-  # ***************
   # UNCOMMENT AND EDIT THE CODE BELOW TO CALCULATE THE SSD VALUES BETWEEN:
   # 1) THE ORIGINAL 16 BIT CT IMAGE AND THE TRANSFORMED 16 BIT CT IMAGE
+  ssd_1 = calcSSD(ct_source_img_16,ct_img_int16_resamp)
   # 2) THE ORIGINAL 16 BIT CT IMAGE AND THE TRANSFORMED 8 BIT CT IMAGE
+  ssd_2 = calcSSD(ct_source_img_16,ct_img_int8_resamp)
   # 3) THE ORIGINAL 16 BIT CT IMAGE AND THE TRANSFORMED MR IMAGE
+  ssd_3 = calcSSD(ct_source_img_16,mr_img_int16_resamp)
   # 4) THE ORIGINAL 8 BIT CT IMAGE AND THE TRANSFORMED 8 BIT CT IMAGE
-  #SSDs[n, 0] = 
-  #SSDs[n, 1] = 
-  #SSDs[n, 2] = 
-  #SSDs[n, 3] = 
-  # ***************
+  ssd_4 = calcSSD(ct_source_img_8,ct_img_int8_resamp)
+
+  SSDs[n, 0] = ssd_1
+  SSDs[n, 1] = ssd_2
+  SSDs[n, 2] = ssd_3
+  SSDs[n, 3] = ssd_4
+
+print(SSDs)
   
 #%%
 # plot the SSD values (y axis) against the angle theta (x axis)
@@ -115,21 +125,29 @@ plt.title('SSD:  8-bit CT and  8-bit CT')
 # 4) THE ORIGINAL 8 BIT CT IMAGE AND THE TRANSFORMED 8 BIT CT IMAGE
 from utils2 import calcMSD
 MSDs = np.zeros((theta.size, 4))
+
 for n in range(theta.size):
   
   # CREATE AFFINE MATRIX AND CORRESPONDING DEFORMATION FIELD
-  aff_mat = 
-  def_field = 
+  aff_mat = affineMatrixForRotationAboutPoint(n, rotation_point)
+  def_field = defFieldFromAffineMatrix(aff_mat, num_pix_x, num_pix_y)
+
   # RESAMPLE THE IMAGES
-  ct_img_int8_resamp = 
-  ct_img_int16_resamp = 
-  mr_img_int16_resamp = 
+  ct_img_int8_resamp = resampImageWithDefField(ct_source_img_8, def_field)
+  ct_img_int16_resamp = resampImageWithDefField(ct_source_img_16, def_field)
+  mr_img_int16_resamp = resampImageWithDefField(mr_source_img, def_field)
   
-  # CALCULATE THE MSD VALUES
-  MSDs[n, 0] = 
-  MSDs[n, 1] = 
-  MSDs[n, 2] = 
-  MSDs[n, 3] = 
+  #calculate msd values
+  msd_1 = calcMSD(ct_source_img_16, ct_img_int16_resamp)
+  msd_2 = calcMSD(ct_source_img_16, ct_img_int8_resamp)
+  msd_3 = calcMSD(ct_source_img_16, mr_img_int16_resamp)
+  msd_4 = calcMSD(ct_source_img_8, ct_img_int8_resamp)
+
+  # create msd matrix
+  MSDs[n, 0] = msd_1
+  MSDs[n, 1] = msd_2
+  MSDs[n, 2] = msd_3
+  MSDs[n, 3] = msd_4
   
 # plot the MSD values (y axis) against the angle theta (x axis)
 plt.figure()
@@ -158,38 +176,41 @@ plt.title('MSD:  8-bit CT and  8-bit CT')
 # 4) THE ORIGINAL 8 BIT CT IMAGE AND THE TRANSFORMED 8 BIT CT IMAGE
 from utils2 import calcNCC
 from utils2 import calcEntropies
+
 NCCs = np.zeros((theta.size, 4))
 H_ABs = np.zeros((theta.size, 4))
 H_As = np.zeros((theta.size, 4))
 H_Bs = np.zeros((theta.size, 4))
+
 for n in range(theta.size):
   
   # CREATE AFFINE MATRIX AND CORRESPONDING DEFORMATION FIELD
-  aff_mat = 
-  def_field = 
+  aff_mat = affineMatrixForRotationAboutPoint(n, rotation_point)
+  def_field = defFieldFromAffineMatrix(aff_mat, num_pix_x, num_pix_y)
+
   # RESAMPLE THE IMAGES
-  ct_img_int8_resamp = 
-  ct_img_int16_resamp = 
-  mr_img_int16_resamp = 
+  ct_img_int8_resamp = resampImageWithDefField(ct_source_img_8, def_field)
+  ct_img_int16_resamp = resampImageWithDefField(ct_source_img_16, def_field)
+  mr_img_int16_resamp = resampImageWithDefField(mr_source_img, def_field)
   
   # CALCULATE THE NCC VALUES
-  NCCs[n, 0] = 
-  NCCs[n, 1] = 
-  NCCs[n, 2] = 
-  NCCs[n, 3] = 
+  NCCs[n, 0] = calcNCC(ct_source_img_16, ct_img_int16_resamp)
+  NCCs[n, 1] = calcNCC(ct_source_img_16, ct_img_int8_resamp)
+  NCCs[n, 2] = calcNCC(ct_source_img_16, mr_img_int16_resamp)
+  NCCs[n, 3] = calcNCC(ct_source_img_8, ct_img_int8_resamp)
   
   # CALCULATE THE JOINT AND MARGINAL ENTROPIES
-  H_ABs[n, 0], H_As[n, 0], H_Bs[n, 0]  = 
-  H_ABs[n, 1], H_As[n, 1], H_Bs[n, 1]  = 
-  H_ABs[n, 2], H_As[n, 2], H_Bs[n, 2]  = 
-  H_ABs[n, 3], H_As[n, 3], H_Bs[n, 3]  = 
+  H_ABs[n, 0], H_As[n, 0], H_Bs[n, 0] = calcEntropies(ct_source_img_16, ct_img_int16_resamp)
+  H_ABs[n, 1], H_As[n, 1], H_Bs[n, 1] = calcEntropies(ct_source_img_16, ct_img_int8_resamp)
+  H_ABs[n, 2], H_As[n, 2], H_Bs[n, 2] = calcEntropies(ct_source_img_16, mr_img_int16_resamp)
+  H_ABs[n, 3], H_As[n, 3], H_Bs[n, 3] = calcEntropies(ct_source_img_8, ct_img_int8_resamp)
 # ***************  
   
 # ***************
 # EDIT THE CODE BELOW TO CALCULATE THE MUTUAL INFORMATION AND NORMALISED MUTUAL INFORMATION
 # (note - this code is outside of the for loop)
-MIs = 
-NMIs = 
+MIs = H_As + H_Bs - H_ABs
+NMIs = (H_As + H_Bs)/H_ABs
 # *************** 
   
 # plot the results for NCC, H_AB, MI, and NMI in separate figures
@@ -248,3 +269,5 @@ plt.title('NMI:  16-bit CT and 16-bit MR')
 plt.subplot(2,2,4)
 plt.plot(theta, NMIs[:, 3])
 plt.title('NMI:  8-bit CT and  8-bit CT')
+
+# %%
